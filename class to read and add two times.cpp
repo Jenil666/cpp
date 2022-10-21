bcdb51280f7b2de96dc45a86f3a87897f9c00443
Jenil666 <116253963+Jenil666@users.noreply.github.com>
2022-10-20 23:38:43 -0700

    Add files via upload

---
+++ class to read and add two times.cpp
@@ -0,0 +1,22 @@
+// WAP to create a class to read and add two times. (hour,minute,secound)
+#include<iostream>
+using namespace std;
+ class a{
+ 	public:
+ 	void b()
+ 	{
+ 	  	int a,h,m,s;
+ 	  	cout<<"Please Enter Your hour";
+		cin>>a;
+		m=a*60;
+		s=m*60;
+		cout<<"Ans="<<a<<":"<<m<<":"<<s;   
+	}
+ };
+ 
+ int main()
+ {
+ 	a a;
+ 	a.b();
+ 	return 0;
+ }
