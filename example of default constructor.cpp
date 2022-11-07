066023069f12f17ea4111b758f0d9057f2cc6c32
Jenil666 <116253963+Jenil666@users.noreply.github.com>
2022-11-07 12:22:54 +0530

    Add files via upload

---
+++ example of default constructor.cpp
@@ -0,0 +1,19 @@
+//4. WAP to demonstrate example of default constructor or no argument constructor.
+
+
+
+
+#include<iostream>
+using namespace std;
+class emp{
+	public:
+		emp()
+		{
+			cout<<"Hellow";
+		}
+};
+int main ()
+{
+	emp e;
+	return 0;
+}
